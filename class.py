75c22b1903eadb0151f0fd1c81962b70c0ae6e2d
Wasikemarvine <jayelwasike@gmail.com>
2016-10-31 17:18:34 +0300

    Add files via upload
    class OOP

---
+++ class.py
@@ -0,0 +1,17 @@
+class wholwsale(object):
+	def _init_(self,amount,item,price,networth=15000):
+		self.amount=amount
+		self.item=item
+		self.price=price
+	
+	def selling(self,amount):
+		self.Balance = (amount)
+	def buying (self,amount):
+		if (self.Balance - amount < - networth):
+			return False
+		else:
+			self.Balance  -=amount
+			return True
+
+	def balance(self):
+		return self.Balance
